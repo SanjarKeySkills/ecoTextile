51b2ac93fbe111b3c1d7ecf8b64096bf3a4818c3
SanjarKeySkills <forest2004@mail.ru>
2025-03-14 12:49:55 +0600

    14/03/25| added Hero block

--- src/components/widgets/HeroBlock/HeroBlock.jsx
+++ src/components/widgets/HeroBlock/HeroBlock.jsx
@@ -1,8 +1,29 @@
 import React from "react";
 import styles from "./heroBlock.module.scss";
+import imageTop from "../../../assets/gallery/heroTop.png";
 
 const HeroBlock = () => {
-    return <div className={styles.heroBlock}> Test 123</div>;
+    return (
+        <div className={styles.heroBlock}>
+            <div className={styles.heroWrapper}>
+                <div className={styles.heroContainer}>
+                    <div className={styles.heroImg}>
+                        <img
+                            src={imageTop}
+                            alt="heroTopImage"
+                            className={styles.imageTop}
+                        />
+                    </div>
+                    <div className={styles.heroTitle}>
+                        <p className={styles.heroTitleText}>
+                            ЭФФЕКТИВНАЯ <br />
+                            Переработка отходов швейной отрасли
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 };
 
 export default HeroBlock;
