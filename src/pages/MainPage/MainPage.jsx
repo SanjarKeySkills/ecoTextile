d80ce9201a793cd9da56a5682fb6c49e7a764f6d
SanjarKeySkills <forest2004@mail.ru>
2025-03-14 15:09:54 +0600

    14.03.25/ Added Media Block. Completed

--- src/pages/MainPage/MainPage.jsx
+++ src/pages/MainPage/MainPage.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import HeroBlock from "../../components/widgets/HeroBlock/HeroBlock.jsx";
+import MediaBlock from "../../components/widgets/MediaBlock/MediaBlock.jsx";
 import WelcomeBlock from "../../components/widgets/WelcomeBlock/WelcomeBlock.jsx";
+import InfotableBlock from "../../components/widgets/InfotableBlock/InfotableBlock.jsx";
 
 const MainPage = () => {
     return (
         <div>
             <HeroBlock />
+            <MediaBlock />
             <WelcomeBlock />
+            <InfotableBlock />
         </div>
     );
 };
