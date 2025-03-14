4db15a6122cb5a8f273773a8268526bcc2245402
SanjarKeySkills <forest2004@mail.ru>
2025-03-14 13:08:20 +0600

    14/03/25| WelcomeBlock completed

--- src/pages/MainPage/MainPage.jsx
+++ src/pages/MainPage/MainPage.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import HeroBlock from "../../components/widgets/HeroBlock/HeroBlock.jsx";
+import WelcomeBlock from "../../components/widgets/WelcomeBlock/WelcomeBlock.jsx";
 
 const MainPage = () => {
     return (
         <div>
             <HeroBlock />
+            <WelcomeBlock />
         </div>
     );
 };
