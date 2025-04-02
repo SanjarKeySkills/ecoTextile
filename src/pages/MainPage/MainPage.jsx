71f1ca03ddec24b965ff75b74fa293c2d543a0f7
SanjarKeySkills <forest2004@mail.ru>
2025-04-02 11:25:09 +0600

    02/04/25| Default_ok

--- src/pages/MainPage/MainPage.jsx
+++ src/pages/MainPage/MainPage.jsx
@@ -1,18 +1,18 @@
 import React from "react";
 import HeroBlock from "../../components/widgets/HeroBlock/HeroBlock.jsx";
-import MediaBlock from "../../components/widgets/MediaBlock/MediaBlock.jsx";
-import WelcomeBlock from "../../components/widgets/WelcomeBlock/WelcomeBlock.jsx";
-import InfotableBlock from "../../components/widgets/InfotableBlock/InfotableBlock.jsx";
-import PartnersBlock from "../../components/widgets/PartnersBlock/PartnersBlock.jsx";
+// import MediaBlock from "../../components/widgets/MediaBlock/MediaBlock.jsx";
+// import WelcomeBlock from "../../components/widgets/WelcomeBlock/WelcomeBlock.jsx";
+// import InfotableBlock from "../../components/widgets/InfotableBlock/InfotableBlock.jsx";
+// import PartnersBlock from "../../components/widgets/PartnersBlock/PartnersBlock.jsx";
 
 const MainPage = () => {
     return (
         <div>
             <HeroBlock />
-            <MediaBlock />
-            <WelcomeBlock />
+            {/* <MediaBlock /> */}
+            {/* <WelcomeBlock />
             <InfotableBlock />
-            <PartnersBlock />
+            <PartnersBlock /> */}
         </div>
     );
 };
