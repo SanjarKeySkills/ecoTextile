c132aa51ae87927b675958b67f66c9d22651f9b6
SanjarKeySkills <forest2004@mail.ru>
2025-04-05 23:28:33 +0600

    05/04/25| manifest resp completed

--- src/components/widgets/MediaBlock/MediaBlock.jsx
+++ src/components/widgets/MediaBlock/MediaBlock.jsx
@@ -11,6 +11,21 @@ const MediaBlock = () => {
     return (
         <div className={styles.mediaBlock}>
             <div className={styles.mediaContainer}>
+                <div className={styles.mediaContent}>
+                    <p className={styles.mediaText}>
+                        "ECOTEXTILEPLUS" - лидер в области переработки отходов
+                        швейной отрасли.
+                        <br /> <br />
+                        Используя экологически безопасные решения по переработке
+                        текстильных отходов, компания производит товары
+                        хозяйственного и бытового назначения.
+                        <br /> <br />
+                        Проект направлен на улучшение экологической ситуации,
+                        снижение объема сжигания отходов швейной отрасли,
+                        уменьшение воздействия на окружающую среду, обеспечивая
+                        будущее без отходов.
+                    </p>
+                </div>
                 <div className={styles.videoWrapper}>
                     <video
                         src={videoBg}
@@ -19,29 +34,10 @@ const MediaBlock = () => {
                         ref={videoRef}
                         className={styles.videoBlock}></video>
                     <p className={styles.heroManifest}>
-                        ДОВЕРЯЙТЕ ПЕРЕРАБОТКУ ПРОФЕССИОНАЛАМ
+                        ДОВЕРЯЙТЕ ПЕРЕРАБОТКУ <br /> ПРОФЕССИОНАЛАМ
                     </p>
                 </div>
-
-                {/* <div className={styles.mediaContent}>
-                    <p className={styles.mediaText}>
-                        "ECOTEXTILEPLUS" - лидер в области переработки отходов
-                        швейной отрасли.
-                        <br /> <br />
-                        Предлагая экологически безопасные решения по переработке
-                        текстильных отходов компания производит товары
-                        хозяйственного и бытового назначения.
-                        <br /> <br />
-                        Проект направлен на улучшение экологической ситуации,
-                        снижение объема сжигания отходов швейной отрасли,
-                        уменьшение воздействия на окружающую среду, обеспечивая
-                        будущее без отходов.
-                    </p>
-                </div> */}
             </div>
-            {/* <div className={styles.manifestBlock}>
-               
-            </div> */}
             <div />
         </div>
     );
