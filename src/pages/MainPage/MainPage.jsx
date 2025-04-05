f65a9762093ca72b01072ad7a006587bee1e2258
SanjarKeySkills <forest2004@mail.ru>
2025-04-05 23:32:06 +0600

    05/04/25| WelcomeBlock activated

--- src/pages/MainPage/MainPage.jsx
+++ src/pages/MainPage/MainPage.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import HeroBlock from "../../components/widgets/HeroBlock/HeroBlock.jsx";
 import MediaBlock from "../../components/widgets/MediaBlock/MediaBlock.jsx";
-// import WelcomeBlock from "../../components/widgets/WelcomeBlock/WelcomeBlock.jsx";
+import WelcomeBlock from "../../components/widgets/WelcomeBlock/WelcomeBlock.jsx";
 // import InfotableBlock from "../../components/widgets/InfotableBlock/InfotableBlock.jsx";
 // import PartnersBlock from "../../components/widgets/PartnersBlock/PartnersBlock.jsx";
 
@@ -10,8 +10,8 @@ const MainPage = () => {
         <div>
             <HeroBlock />
             <MediaBlock />
-            {/* <WelcomeBlock />
-            <InfotableBlock />
+            <WelcomeBlock />
+            {/* <InfotableBlock />
             <PartnersBlock /> */}
         </div>
     );
