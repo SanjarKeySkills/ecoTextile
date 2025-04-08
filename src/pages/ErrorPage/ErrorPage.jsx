c23fbd16a3618c3f93055afef753f785e58d5249
SanjarKeySkills <forest2004@mail.ru>
2025-04-08 16:07:48 +0600

    08/04/25| errorpage ok

--- src/pages/ErrorPage/ErrorPage.jsx
+++ src/pages/ErrorPage/ErrorPage.jsx
@@ -1,7 +1,13 @@
 import React from "react";
+import styles from "./errorPage.module.scss";
+import errorImg from "../../assets/404-error.png";
 
 const ErrorPage = () => {
-    return <div>Test</div>;
+    return (
+        <div className={styles.errorPage}>
+            <img src={errorImg} alt="errorImg" />
+        </div>
+    );
 };
 
 export default ErrorPage;
